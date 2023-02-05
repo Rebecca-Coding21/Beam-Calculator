e74ec0365f46e15af93d1149efc3f90e0f23a674
Rebecca-Coding21 <94243632+Rebecca-Coding21@users.noreply.github.com>
2023-02-05 18:26:45 +0100

    Add calculation for different spans

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import os
-
+from helpers import apology
+import csv
+import sys
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 from flask import Flask, redirect, render_template, flash, request
 
 app = Flask(__name__)
@@ -24,25 +29,133 @@ def calculation():
     if request.method == "POST":
         # Check for input errors
 
-        #if not request.form.get("span"):
-           # return apology()
-        #elif not request.form.get("fields"):
-           # return apology()
-        #elif not request.form.get("load"):
-            #return apology()
+        if not request.form.get("span"):
+            return apology("Please enter a value for span!")
+        elif not request.form.get("fields"):
+            return apology("Please select a number of spans!")
+        elif not request.form.get("load"):
+            return apology("Please enter a load value!")
 
-        n = request.form.get("fields")      # number of fields selected by the user (1-5)
-        l = float(request.form.get("span")) # pan in meters
-        q = float(request.form.get("load")) # constant line-load in kN/m
+        n = int(request.form.get("fields")) # number of fields selected by the user (1-5)
+        length = request.form.get("span") # span in meters
+        l = float(length.replace(",", "."))
+        load = request.form.get("load") # constant line-load in kN/m
+        q = float(load.replace(",", "."))
+        
+        momentfactorsData = []
 
-        #if amountOfFields == 1:
-        shearForce = (q * l)/2              # Shear Force at both supports in kN
-        bendingMomentField = (q * l**2)/8   # Bending - Moment in middle of field in kNm
-        #else:
-            #shearForce
+        with open("momentfactors.csv") as momentfactorFile:
+            momentfactorsData = csv.reader(momentfactorFile)
+            momentfactors = list(momentfactorsData)
+        
+        m1_factor = float(momentfactors[n][1])
+        m2_factor = float(momentfactors[n][2])
+        m3_factor = float(momentfactors[n][3])
+        mb_factor = float(momentfactors[n][4])
+        mc_factor = float(momentfactors[n][5])
+        A_factor = float(momentfactors[n][6])
+        B_factor = float(momentfactors[n][7])
+        C_factor = float(momentfactors[n][8])
+        Vbl_factor = float(momentfactors[n][9])
+        Vbr_factor = float(momentfactors[n][10])     
+        Vcl_factor = float(momentfactors[n][11])  
+        Vcr_factor = float(momentfactors[n][12])            
+        M1 = 0.0
+        M2 = 0.0
+        M3 = 0.0
+        Mb = 0.0
+        Mc = 0.0
+        A = 0.0
+        B = 0.0
+        C = 0.0
+        Vbl = 0.0
+        Vbr = 0.0
+        Vcl = 0.0
+        Vcr = 0.0
 
-        return render_template("calculated.html", span = l, load = q, fields = n, shearForce = shearForce, bendingMomentField = bendingMomentField)
+        if n == 1:
+            Vbl = (q * l)/2     # Shear Force at both supports in kN
+            M1 = (q * l**2)/8   # Bending - Moment in middle of field in kNm
+        else:
+            M1 = round(m1_factor * q * l**2,2)
+            M2 = round(m2_factor * q * l**2,2)
+            M3 = round(m3_factor * q * l**2,2)
+            Mb = round(mb_factor * q * l**2,2)
+            Mc = round(mc_factor * q * l**2,2)
+            A = round(A_factor * q * l,2)
+            B = round(B_factor * q * l,2)
+            C = round(C_factor * q * l,2)
+            Vbl = round(Vbl_factor * q * l,2)
+            Vbr = round(Vbr_factor * q * l,2)
+            Vcl = round(Vbr_factor * q * l,2)
+            Vcr = round(Vbr_factor * q * l,2)
 
+        # list of moments
+        moments = []
+        if n == 1:
+            moments = [0.0, M1, 0.0]
+        elif n == 2:
+            moments = [0.0, M1, Mb, M1, 0.0]
+        elif n == 3:
+            moments = [0.0, M1, Mb, M2, Mb, M1, 0.0]
+        elif n == 4:
+            moments = [0.0, M1, Mb, M2, Mc, M2, Mb, M1, 0.0]
+        elif n == 5:
+            moments = [0.0, M1, Mb, M2, Mc, M3, Mc, M2, Mb, M1, 0.0]
+        
+        #list of shear forces
+        shearForces = []
+        if n == 1:
+            shearForces = [A, 0, -A]
+        elif n == 2:
+            shearForces = [A, 0, -B/2, B/2, 0, -A] # two values at one x-location!!! 
+        elif n == 3:
+            shearForces = [A, 0, -B/2, B/2, 0, -B/2, B/2, 0, -A]
+        elif n == 4:
+            shearForces = [A, 0, -B/2, B/2, 0, -C/2, C/2, 0, -B/2, B/2, 0, -A]
+        elif n == 5:
+            shearForces = [A, 0, -B/2, B/2, 0, -C/2, C/2, 0, -C/2, C/2, 0, -B/2, B/2, 0, -A]
+        
+        print(moments)
+                
+        #List for x-locations
+        x_location = []
+
+        for i in range(n * 2 + 1):
+            x = i/2 * l
+            x_location.append(x)
+        
+        print(x_location)
+        
+        #Result Graph:
+        #Plot(x_location, moments)
+
+        csv_results = [M1, M2, M3, Mb, Mc, A, B, C, Vbl, Vbr, Vcl, Vcr]
+
+        #with open(results.csv, "w") as r:
+         #   writer = csv.writer(r)
+          #  writer.writerow(csv_results)
+
+        return render_template("calculated.html", span = l, load = q, fields = n, Vbl = Vbl, Vbr = Vbr, Vcl = Vcl, Vcr = Vcr, M1 = M1, M2 = M2, M3 = M3, Mb = Mb, Mc = Mc, A = A, B = B, C = C)
+        
     else:
         return render_template("index.html")
+
+def Plot(x_location, moments):
+    plt.plot(x_location, moments)
+    plt.show()
+    plt.savefig(sys.stdout.buffer)
+    sys.stdout.flush()
+
+@app.route('/csv', methods=["GET", "POST"])
+def CreateCsv():
+    
+    if request.method == "POST":
+        #with open(results.csv, "w") as r:
+         #   writer = csv.writer(r)
+          #  writer.writerow(csv_results)
+        
+        return render_template("csv.html")
+
+
 
